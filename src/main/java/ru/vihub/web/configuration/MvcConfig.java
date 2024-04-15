23c720b17ff38f615d440b0f7fe0096a76dbaea0
wannacry <lobanov4747@gmail.com>
2024-04-15 15:45:51 +0300

    Добавлен profile page, стандартный view controller для "/profile" endpoint

--- src/main/java/ru/vihub/web/configuration/MvcConfig.java
+++ src/main/java/ru/vihub/web/configuration/MvcConfig.java
@@ -17,6 +17,7 @@ public class MvcConfig implements WebMvcConfigurer {
             registry.addViewController(url).setViewName("home");
         }
         registry.addViewController("/recommendations").setViewName("recommendations");
+        registry.addViewController("/profile").setViewName("profile");
         registry.addViewController("/login").setViewName("login");
     }
 }
