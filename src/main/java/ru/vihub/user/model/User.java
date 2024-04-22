4e905b6ef328aa8bd336cd5fff0aee40bc160260
Cishy <fedorovnikita04@yandex.ru>
2024-04-22 20:05:50 +0300

    Удалены неиспользуемые импорты

--- src/main/java/ru/vihub/user/model/User.java
+++ src/main/java/ru/vihub/user/model/User.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.UUID;
 
 @Entity
 @Getter
