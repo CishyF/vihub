0eb802dadc0c9e770a9b1e9b4edacf9f96b5d668
Cishy <fedorovnikita04@yandex.ru>
2024-04-26 16:17:36 +0300

    Удалены неиспользуемые методы и импорты

--- src/main/java/ru/vihub/security/service/AuthenticationService.java
+++ src/main/java/ru/vihub/security/service/AuthenticationService.java
@@ -1,12 +1,8 @@
 package ru.vihub.security.service;
 
-import org.springframework.security.core.userdetails.UserDetailsService;
-import ru.vihub.security.dto.LoginDtoRequest;
 import ru.vihub.security.dto.RegistrationDtoRequest;
 
 public interface AuthenticationService{
 
     void register(RegistrationDtoRequest dto);
-
-    void login(LoginDtoRequest dto);
 }
--- src/main/java/ru/vihub/security/service/AuthenticationServiceImpl.java
+++ src/main/java/ru/vihub/security/service/AuthenticationServiceImpl.java
@@ -1,18 +1,9 @@
 package ru.vihub.security.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ru.vihub.security.dto.LoginDtoRequest;
 import ru.vihub.security.dto.RegistrationDtoRequest;
-import ru.vihub.user.model.User;
 import ru.vihub.user.service.UserService;
-
-import java.util.Optional;
-
 
 @Service
 @RequiredArgsConstructor
@@ -23,10 +14,5 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public void register(RegistrationDtoRequest dto) {
         userService.createUser(dto);
-    }
-
-    @Override
-    public void login(LoginDtoRequest dto) {
-
     }
 }
