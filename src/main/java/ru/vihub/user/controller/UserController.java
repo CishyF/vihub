c928cb7f0a415d149c8054c816bbdbac04a30811
ScarFace163 <rarman321123@gmail.com>
2024-04-26 18:05:25 +0300

    GetMapping перенесен в UserController

--- src/main/java/ru/vihub/user/controller/UserController.java
+++ src/main/java/ru/vihub/user/controller/UserController.java
@@ -1,8 +1,21 @@
 package ru.vihub.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.vihub.user.model.User;
 
 @Controller
-public class UserController {
+@RequiredArgsConstructor
 
+
+public class UserController {
+    @GetMapping("/profile")
+    public String getProfile(Model model){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user",(User)principal);
+        return "profile";
+    }
 }
