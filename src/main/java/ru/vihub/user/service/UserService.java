f286995f03815294f794ee3d5bcd56c2f4cfc2b1
ScarFace163 <rarman321123@gmail.com>
2024-05-03 19:40:36 +0300

    Изменины сервис и репозиторий User

--- src/main/java/ru/vihub/user/service/UserService.java
+++ src/main/java/ru/vihub/user/service/UserService.java
@@ -2,14 +2,15 @@ package ru.vihub.user.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 import ru.vihub.security.dto.RegistrationDtoRequest;
+import ru.vihub.user.dto.UserDto;
 import ru.vihub.user.model.User;
 
 public interface UserService{
-
-
     User findByUsername(String username);
+    UserDto findById(long id);
 
     User createUser(RegistrationDtoRequest dto);
 
     UserDetailsService userDetailsService();
+    void updateUser(UserDto userDto) ;
 }
--- src/main/java/ru/vihub/user/service/UserServiceImpl.java
+++ src/main/java/ru/vihub/user/service/UserServiceImpl.java
@@ -1,16 +1,25 @@
 package ru.vihub.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.vihub.security.dto.RegistrationDtoRequest;
 import ru.vihub.exception.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import ru.vihub.user.dto.UserDto;
 import ru.vihub.user.model.User;
 import ru.vihub.user.repository.UserRepository;
 
+
+import static ru.vihub.user.mapper.UserMapper.mapToUser;
+import static ru.vihub.user.mapper.UserMapper.mapToUserDto;
 import static ru.vihub.user.model.Role.ADMIN;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -18,10 +27,16 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+
     @Override
     public User findByUsername(String username) {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new EntityNotFoundException("Пользователь не найден"));
+    }
+
+    @Override
+    public UserDto findById(long id) {
+        return mapToUserDto(userRepository.findById(id) .orElseThrow(() -> new EntityNotFoundException("Пользователь не найден")));
     }
 
     @Override
@@ -40,5 +55,22 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDetailsService userDetailsService() {
         return this::findByUsername;
+    }
+
+    @Override
+    public void updateUser(UserDto userDto) {
+        User user = mapToUser(userDto);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userPrevious = (User)principal;
+        user.setId(userPrevious.getId());
+        user.setRole(userPrevious.getRole());
+        user.setPassword(userPrevious.getPassword());
+        log.info("user{}", user);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        ((User) principal).setUsername(user.getUsername());
+        ((User) principal).setPassword(user.getPassword());
+        Authentication newAuth = new UsernamePasswordAuthenticationToken(principal, auth.getCredentials(), auth.getAuthorities());
+        userRepository.save(user);
+        SecurityContextHolder.getContext().setAuthentication(newAuth);
     }
 }
