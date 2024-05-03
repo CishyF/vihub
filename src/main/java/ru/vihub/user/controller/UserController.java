a4a3f6fb444e365637f9a32d96b28c3a0cdf2874
ScarFace163 <rarman321123@gmail.com>
2024-05-03 19:39:45 +0300

    Изменен UserController. Добавлен маппинг для редактирования профиля

--- src/main/java/ru/vihub/user/controller/UserController.java
+++ src/main/java/ru/vihub/user/controller/UserController.java
@@ -1,30 +1,60 @@
 package ru.vihub.user.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.vihub.user.dto.UserDto;
 import ru.vihub.user.model.User;
+import ru.vihub.user.service.UserService;
 
 @Controller
 @RequiredArgsConstructor
-
-
+@Slf4j
 public class UserController {
+    private final UserService userService;
+
     @GetMapping("/profile")
     public String getProfile(Model model){
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("user",(User)principal);
+        model.addAttribute("user",findCurrentUser());
         return "profile";
     }
     @PostMapping("/profile")
     public String logOut(HttpServletRequest request) throws ServletException {
         request.logout();
         return "redirect:/auth/login";
+    }
+    @GetMapping("/profile/edit")
+    public String getProfileEdit(Model model){
+
+        model.addAttribute("user",findCurrentUser());
+        return "profile-edit";
+    }
+    @PostMapping("/profile/edit")
+    public String updateProfile(@Valid @ModelAttribute("user")UserDto userDto,
+                                BindingResult result, Model model)  {
+        if (result.hasErrors()){
+            model.addAttribute("user",userDto);
+            return "profile-edit";
+        }
+        log.info("userDto {}",userDto);
+        userService.updateUser(userDto);
+        return "redirect:/profile";
+    }
+    private UserDto findCurrentUser(){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = (User) principal;
+        long id = user.getId();
+        return userService.findById(id);
     }
 }
 
