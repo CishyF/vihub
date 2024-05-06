937234286f63e4938384372397de0bc04b9dc58c
ScarFace163 <rarman321123@gmail.com>
2024-05-06 15:29:44 +0300

    Установлен стиль кода google-java-format

--- src/main/java/ru/vihub/user/dto/UserDto.java
+++ src/main/java/ru/vihub/user/dto/UserDto.java
@@ -9,32 +9,33 @@ import lombok.Data;
 @Data
 @Builder
 public class UserDto {
-    private long id;
-    @Size(min = 1, message = "Username must contains at least 1 symbol!")
-    @Size(max = 20, message = "Username must contains less than 20 symbols!")
-    @Pattern(regexp = "^[A-Za-z][A-Za-z0-9_]{1,20}$", message = "Username is incorrect, try another!")
-    private String username;
+  private long id;
 
-    @Size(min = 4, message = "Password must contains at least 4 symbol!")
-    @Size(max = 20, message = "Password must contains less than 20 symbols!")
-    @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "Password is incorrect, try another!")
-    private String password;
+  @Size(min = 1, message = "Username must contains at least 1 symbol!")
+  @Size(max = 20, message = "Username must contains less than 20 symbols!")
+  @Pattern(regexp = "^[A-Za-z][A-Za-z0-9_]{1,20}$", message = "Username is incorrect, try another!")
+  private String username;
 
-    @Email(message = "Email is incorrect, try again!")
-    private String email;
+  @Size(min = 4, message = "Password must contains at least 4 symbol!")
+  @Size(max = 20, message = "Password must contains less than 20 symbols!")
+  @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "Password is incorrect, try another!")
+  private String password;
 
-    @Pattern(regexp = "^[A-Za-z]{2,30}$", message = "Firstname is incorrect, try another!")
-    private String firstname;
+  @Email(message = "Email is incorrect, try again!")
+  private String email;
 
-    @Pattern(regexp = "^[A-Za-z]{2,30}$", message = "Lastname is incorrect, try another!")
-    private String lastname;
+  @Pattern(regexp = "^[A-Za-z]{2,30}$", message = "Firstname is incorrect, try another!")
+  private String firstname;
 
-    @Pattern(regexp = "^((\\+7|7|8)+([0-9]){10})$")
-    private String phone;
+  @Pattern(regexp = "^[A-Za-z]{2,30}$", message = "Lastname is incorrect, try another!")
+  private String lastname;
 
-    private String address;
-    private String github;
-    private String instagram;
-    private String facebook;
-    private String photoUrl;
+  @Pattern(regexp = "^[+]{1}(?:[0-9\\-()/.]\\s?){6,15}[0-9]{1}$", message = "Phone is incorrect")
+  private String phone;
+
+  private String address;
+  private String github;
+  private String instagram;
+  private String facebook;
+  private String photoUrl;
 }
