7be3decb46dc3383fbd3e8b568d52c68d4f2167a
ScarFace163 <rarman321123@gmail.com>
2024-05-11 16:46:59 +0300

    Убран лишний import

--- src/main/java/ru/vihub/security/controller/RegistrationController.java
+++ src/main/java/ru/vihub/security/controller/RegistrationController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.vihub.security.dto.RegistrationDtoRequest;
 import ru.vihub.security.service.AuthenticationService;
